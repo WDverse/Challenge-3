81466070d0ee1c01a9259bbd35aa1279588219cd
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-05-15 17:27:26 -0400

    added an object and a function to js file

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordCharacters = {
+  numCharacters: "0123456789",
+  specialCharacters: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}
+
+function generatePassword() {
+  const passwordLength = window.prompt("How many characters do you want the password to contain?")
+  const acceptSpecialCharacters = confirm("Click OK to confirm including special characters")
+  const acceptnum = confirm("Click OK to confirm including numeric characters")
+  const acceptlower = confirm("Click OK to confirm including lowercase")
+  const acceptUpper = confirm("Click OK to confirm including uppercase")
+}
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
@@ -12,3 +32,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
