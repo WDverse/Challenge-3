b197fb8cd6d801b03cce6169bdb91d1da4e94680
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-05-16 01:04:04 -0400

    updated js file

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordCharacters = {
-  numCharacters: "0123456789",
-  specialCharacters: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-}
 
-function generatePassword() {
-  const passwordLength = window.prompt("How many characters do you want the password to contain?")
-  const acceptSpecialCharacters = confirm("Click OK to confirm including special characters")
-  const acceptnum = confirm("Click OK to confirm including numeric characters")
-  const acceptlower = confirm("Click OK to confirm including lowercase")
-  const acceptUpper = confirm("Click OK to confirm including uppercase")
-}
+// function generatePassword() {
+//   const passwordLength = window.prompt("How many characters do you want the password to contain?");
+//   const acceptSpecialCharacters = confirm("Click OK to confirm including special characters");
+//   const acceptNum = confirm("Click OK to confirm including numeric characters");
+//   const acceptLower = confirm("Click OK to confirm including lowercase");
+//   const acceptUpper = confirm("Click OK to confirm including uppercase");
+// };
+// var passwordCharacters = {
+//   numCharacters: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+//   specialCharacters: ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "["],
+//   lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+//   uppercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+// }
 
+// for (var i = 0; i <= passwordCharacters.length; i++) {
 
-
-
+//   if (acceptSpecialCharacters && acceptNum && acceptLower && acceptUpper) {
+//     return []
+//   }
+// }
 
 // Write password to the #password input
 function writePassword() {
