02d4f6745320b1f14b7b24f1c0e4e069083b4b87
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-05-16 23:26:44 -0400

    added if statements to js file

--- assets/script.js
+++ assets/script.js
@@ -8,19 +8,30 @@ function generatePassword() {
   const acceptNum = confirm("Click OK to confirm including numeric characters");
   const acceptLower = confirm("Click OK to confirm including lowercase");
   const acceptUpper = confirm("Click OK to confirm including uppercase");
+  var master = [];
+  var numCharacters =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "["];
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  if(acceptSpecialCharacters){
+    master = master.concat(specialCharacters)
+  }
+  if(acceptNum){
+    master = master.concat(numCharacters)
+  }
+  if(acceptLower){
+    master = master.concat(lowercase)
+  }
+  if(acceptUpper){
+    master = master.concat(uppercase)
+  }
+
 };
 
 
-var numCharacters =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "["];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-if (acceptSpecialCharacters && acceptNum && acceptLower && acceptUpper){
-  Math.floor(Math.random())
-
-}
 
 
 // Write password to the #password input
