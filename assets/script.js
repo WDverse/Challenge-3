a19ea6ef4704855859ea980026797ee8bef14776
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-05-18 22:32:28 -0400

    added comments to js file

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,26 @@
 var generateBtn = document.querySelector("#generate");
 
 
-function generatePassword() {
+function generatePassword() {  // Declares a function called generatePassword
+
   const passwordLength = parseInt(window.prompt("How many characters do you want the password to contain?"));
-  if (passwordLength > 128 || passwordLength < 8){
+
+  // Sets limits for password length
+  if (passwordLength > 128 || passwordLength < 8){   
     return alert("Password length cannot be lesser than 8 characters or more than 128 characters")
   }else{passwordLength}
+
   const acceptSpecialCharacters = confirm("Click OK to confirm including special characters");
   const acceptNum = confirm("Click OK to confirm including numeric characters");
   const acceptLower = confirm("Click OK to confirm including lowercase");
   const acceptUpper = confirm("Click OK to confirm including uppercase");
-  var master = [];
-  var numCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "["];
-  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var master = []; // Declares an empty array called master
+  var numCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; // Declares a variable called numCharacters
+  var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "["]; // Declares a variable called specialCharacters
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; // Declares a variable called lowercase
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; // Declares a variable called uppercase
 
+  // Uses if statements to concatenate arrays to master variable based on user choice
 
   if (acceptSpecialCharacters) {
     master = master.concat(specialCharacters);
@@ -31,9 +36,11 @@ function generatePassword() {
     master = master.concat(uppercase);
   }
 
-  var passwordArray = []
+  var passwordArray = [] // Declares an empty array called passwordArray
 
-  for (var i = 0; i <= (passwordLength); i++) {
+  //Uses a for loop to randomly select elements in master
+
+  for (var i = 0; i <= passwordLength; i++) {
     var randomIndex = Math.floor(Math.random() * master.length);
     var randomCharacter = master[randomIndex];
     passwordArray.push(randomCharacter);
